1d8853af5ac0620c7e5f9b7d227061eb0ca1d0fd
Justin Li <justinnova0915@gmail.com>
2025-08-29 09:48:41 -0400

    Enabled speed and analytic insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,8 @@ export default function RootLayout({
           <Navbar />
           <main className="flex-grow overflow-y-auto min-h-0">{children}</main>
         </AuthProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
